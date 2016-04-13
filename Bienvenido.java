868c13ecd103d66f46bd89e7222356e280fe9fbe
toblerone554 <toblerone554@gmail.com>
2016-04-13 20:20:22 +0200

    Bienvenido a Java

---
+++ Bienvenido.java
@@ -0,0 +1,13 @@
+//Primer ejercicio Java
+//Programa para imprimir texto
+
+public class Bienvenido
+{
+	//el metodo main empieza aqui
+	public static void main (String args[])
+	{	//Usamos el metodo printl para imprimir texto
+		System.out.println("Bienvenido a Java");
+	}
+		//Fin del método main
+}
+//Fin del programa y de la clase Bienvenido
