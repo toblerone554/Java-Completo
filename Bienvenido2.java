6095321c0f96ec02ff690a64f4ea4d15425db217
toblerone554 <toblerone554@gmail.com>
2016-04-15 10:43:45 +0200

    -Imprimir en varias lineas con una sola instruccion

---
+++ Bienvenido2.java
@@ -0,0 +1,15 @@
+//Programa para probar distintos tipos de impresión
+
+// Creamos una clase pública
+public class Bienvenido2{
+	
+	// Creamos nuestro método main
+	public static void main (String args[]){
+		System.out.print ("Bienvenido"); // No hay retorno de carro
+		System.out.println ("Este es una linea"); // Si hay retorno de carro
+		
+	}
+	//Fin del método main
+}
+
+// Fin de la clase pública
---
+++ Bienvenido3.java
@@ -0,0 +1,13 @@
+// Imprimir varias lineas con una sola instrucción
+
+public class Bienvenido3{
+	
+		// el método main empieza la ejecución de la aplicacion en Java
+		public static void main (String args[]){
+			
+		System.out.println("Bienvenido\na\n la programacion\nen Java¡");
+				
+		}// fin del método main
+	
+
+}
