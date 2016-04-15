94f44966dc47cf3c1f843233a0a205a701bb2618
toblerone554 <toblerone554@gmail.com>
2016-04-15 12:38:59 +0200

    -Imprimir con printf

---
+++ Bienvenido4.java
@@ -0,0 +1,15 @@
+//Imprimir varias lineas en un cuadro de dialogo
+
+//Creamos clase pública
+
+public class Bienvenido4{
+	
+	//El metodo main empieza la ejecucion de la aplicacion de Java
+	public static void main (String args[]){
+	
+		System.out.printf("%s\n%s\n", "Bienvenido a", "la programacion en Java¡");
+		
+	} //Fin metodo main
+
+
+}//Fin de la clase Bienvenido4
